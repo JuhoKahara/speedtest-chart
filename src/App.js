314c1235c1050aae6e9820c7cec9c2514b56a11a
JuhoKahara <juhokaha@metropolia.fi>
2021-09-12 21:45:18 +0300

    created chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import './App.css';
+import results from './speedtest-results';
 
-function App() {
+export default function App() {
+  const BYTE_TO_MBIT = 125000;
+  const data = [];
+  for (let i = 0; i < results.length; i++) {
+    data[i] = {
+      "date": results[i].timestamp,
+      "download": results[i].download.bandwidth / BYTE_TO_MBIT,
+      "upload": results[i].upload.bandwidth / BYTE_TO_MBIT
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LineChart
+      width={ 500 }
+      height={ 300 }
+      data={ data }
+      margin={{
+        top:5,
+        right:30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="timestamp" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line 
+        type="monotone" 
+        dataKey="download" 
+        stroke="#82ca9d" 
+        activeDot={{ r: 8}} 
+      />
+      <Line 
+        type="monotone" 
+        dataKey="upload" 
+        stroke="#8884d8" 
+      />
+    </LineChart>
   );
 }
-
-export default App;
